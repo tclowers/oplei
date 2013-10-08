f8ae6c841a65566a073d46f9265345587c5c0ec8
tom <tclowers@gmail.com>
2013-10-09 02:47:04 +0800

    trying to serve static assests on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Oplei::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  ###### changed to 'true' for use on heroku
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
