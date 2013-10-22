501ad4ca298a49015fd38e60d31a9323af14f982
tom <tclowers@gmail.com>
2013-10-22 22:21:26 +0800

    Make a basic user model with passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Oplei::Application.routes.draw do
+  get "users/new"
   resources :facts
   root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/new_fact',    to: 'facts#new',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131018141118) do
+ActiveRecord::Schema.define(version: 20131022135037) do
 
   create_table "facts", force: true do |t|
     t.string   "question"
@@ -22,5 +22,15 @@ ActiveRecord::Schema.define(version: 20131018141118) do
   end
 
   add_index "facts", ["question"], name: "index_facts_on_question", unique: true, using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
