5cbe8f11922d5127808bd0ec5ae493de077fadb6
tom <tclowers@gmail.com>
2014-01-18 20:08:08 -0800

    add answer_size helper to exams_helper, hshould have branched :P

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -1,2 +1,22 @@
 module ExamsHelper
+	def answer_size(answer)
+		case answer.length
+		when 0..30
+			'small-answer'
+		when 31..48
+			'med-answer'
+		else # answer.length > 48
+			'big-answer'
+		end
+	end
+=begin
+ ## previously in view:
+ <% if choice.answer.length <= 30 %>
+ 	<% answer_size = 'small-answer' %>
+ <% elsif choice.answer.length > 30 && choice.answer.length <= 48 %>
+ 	<% answer_size = 'med-answer' %>
+ <% elsif choice.answer.length > 48 %>
+ 	<% answer_size = 'big-answer' %>
+ <% end %>
+=end
 end
